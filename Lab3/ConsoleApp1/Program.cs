0d20bffa58fb040173aa361ec3bf9004807e10e4
Jinyang Chen <chenjinyang577@gmail.com>
2021-06-13 13:25:33 -0400

    first commit

---
+++ Lab3/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            Circle circle = new Circle();
+
+            Calculate(rect);
+            Calculate(circle);
+            
+        }
+
+        public static void Calculate (Shape1 S)
+        {
+            Console.WriteLine("Area : {0}", S.Area());
+            Console.WriteLine("Circumference : {0}", S.Circumference());
+
+        }
+    }
+}
---
+++ Lab3/ConsoleApp1/Shape1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise1
+{
+    abstract class Shape1
+    {
+        protected float R, L, B;
+        protected float area, circum;
+        public abstract float Area();
+        public abstract float Circumference();
+    }
+
+    class Rectangle: Shape1
+    {
+        public override float Area()
+        {
+            Console.WriteLine("Enter Length: ");
+            L = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Breadth: ");
+            B = int.Parse(Console.ReadLine());
+
+            return L * B;
+        }
+        public override float Circumference()
+        {
+            return (L + B) * 2;
+        }
+    }
+
+    class Circle: Shape1
+    {
+        public override float Area()
+        {
+            Console.WriteLine("Enter Radius: ");
+            R = int.Parse(Console.ReadLine());
+
+            return (float)Math.PI * R * R;
+        }
+
+        public override float Circumference()
+        {
+            return (float)Math.PI * 2 * R;
+        }
+    }
+}
